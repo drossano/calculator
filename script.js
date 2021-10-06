68fb136f944be9751d8a748d8d23074f27fd83c7
drossano <72749793+drossano@users.noreply.github.com>
2021-10-06 19:47:58 -0400

    Add operate function

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ add = (argument1,argument2) => argument1+argument2;
 subtract = (argument1,argument2) => argument1-argument2;
 multiply = (argument1,argument2) => argument1*argument2;
 divide = (argument1,argument2) => argument1/argument2;
+
+function operate (operator,a,b) {
+    if (operator == add){
+      return add(a,b);
+    }else if (operator == subtract) {
+        return subtract(a,b);
+    } else if (operator == multiply) {
+        return multiply(a,b);
+    } else if (operator == divide) {
+        return divide(a,b);
+    }
+}
