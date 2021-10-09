ed7236140e629fcde3890406f78c5998d40ecfaa
drossano <72749793+drossano@users.noreply.github.com>
2021-10-09 16:29:41 -0400

    Add ability to perfom operations on answer

--- script.js
+++ script.js
@@ -2,17 +2,13 @@ function operate (operator,argument1,argument2) {
     argument1 = parseInt(argument1);
     argument2 = parseInt(argument2);
     if (operator == 'add'){
-      let answer =  add(argument1,argument2);
-      return populateDisplay(answer);
+        return add(argument1,argument2);
     }else if (operator == 'subtract') {
-        let answer =  subtract(argument1,argument2);
-        return populateDisplay(answer);
+        return subtract(argument1,argument2);
     } else if (operator == 'multiply') {
-        let answer =  multiply(argument1,argument2);
-        return populateDisplay(answer);
+        return multiply(argument1,argument2);
     } else if (operator == 'divide') {
-        let answer =  divide(argument1,argument2);
-        return populateDisplay(answer);
+        return divide(argument1,argument2);
     }
 }
 
@@ -24,19 +20,35 @@ divide = (argument1,argument2) => argument1/argument2;
 function pressButton () {
     let number = ""
     let operator = "";
-    let argument1 = ""
+    let argument1;
+    let argument2;
     const button = document.querySelectorAll('button');
     button.forEach((button) => {
         button.addEventListener('click', () => {
+            //Below runs when 
             if (button.className == 'number'){
                 number += button.id
                 return populateDisplay(number);
             } else if (button.className == 'operator'){
-                argument1 = number;
-                operator = button.id;
-                number = "";
+                if (argument1 == undefined){
+                    argument1 = number;
+                    operator = button.id;
+                    number = "";
+                } else {
+                    argument2 = number;
+                    operator = button.id;
+                    number = "";
+                }
             } else if (button.id == 'equal'){
-                return operate(operator,argument1,number);
+                if (argument2 == undefined){
+                    let answer = operate(operator,argument1,number); //this result should become argument1
+                    argument1 = answer;
+                    return populateDisplay(answer);
+                } else {
+                    let answer = operate(operator,argument1,argument2); //this result should become argument1
+                    argument1 = answer;
+                    return populateDisplay(answer); 
+                }
             }
         })
     })
