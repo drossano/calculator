178217291296c0f38c723040dab4c3551c7fdd52
drossano <72749793+drossano@users.noreply.github.com>
2021-10-09 10:28:36 -0400

    Add functionality for buttons to run basic operations

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
-add = (argument1,argument2) => argument1+argument2;
+function operate (operator,a,b) {
+    a = parseInt(a);
+    b = parseInt(b);
+    if (operator == 'add'){
+      let answer =  add(a,b);
+      return populateDisplay(answer);
+    }else if (operator == 'subtract') {
+        let answer =  subtract(a,b);
+        return populateDisplay(answer);
+    } else if (operator == 'multiply') {
+        let answer =  multiply(a,b);
+        return populateDisplay(answer);
+    } else if (operator == 'divide') {
+        let answer =  divide(a,b);
+        return populateDisplay(answer);
+    }
+}
+
+add = (argument1,argument2) =>argument1+argument2;
 subtract = (argument1,argument2) => argument1-argument2;
 multiply = (argument1,argument2) => argument1*argument2;
 divide = (argument1,argument2) => argument1/argument2;
 
-function operate (operator,a,b) {
-    if (operator == add){
-      return add(a,b);
-    }else if (operator == subtract) {
-        return subtract(a,b);
-    } else if (operator == multiply) {
-        return multiply(a,b);
-    } else if (operator == divide) {
-        return divide(a,b);
-    }
-}
-
 function pressButton () {
     let number = ""
+    let operator = "";
+    let arg1 = ""
     const button = document.querySelectorAll('button');
     button.forEach((button) => {
         button.addEventListener('click', () => {
@@ -24,7 +32,11 @@ function pressButton () {
                 number += button.id
                 return populateDisplay(number);
             } else if (button.className == 'operator'){
-                return pressOperator(number);
+                arg1 = number;
+                operator = button.id;
+                number = "";
+            } else if (button.id == 'equal'){
+                return operate(operator,arg1,number);
             }
         })
     })
@@ -33,19 +45,6 @@ function pressButton () {
 function populateDisplay (number) {
     const display = document.querySelector('#display');
     display.textContent = number;
-    return pressOperator(number);
-}
-
-function pressOperator (number) {
-    const operatorButtons = document.querySelectorAll('.operator');
-    operatorButtons.forEach((button) =>{
-        button.addEventListener('click', () =>{
-            operation = {
-                operator: button.id,
-                arg1: number,
-            }
-        })
-    });
 }
 
 pressButton();
