e496e5d4abd37260f807513b6f3e1b349d2d6b89
drossano <72749793+drossano@users.noreply.github.com>
2021-10-09 10:33:45 -0400

    Rename argument variables to be more consistent

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-function operate (operator,a,b) {
-    a = parseInt(a);
-    b = parseInt(b);
+function operate (operator,argument1,argument2) {
+    argument1 = parseInt(argument1);
+    argument2 = parseInt(argument2);
     if (operator == 'add'){
-      let answer =  add(a,b);
+      let answer =  add(argument1,argument2);
       return populateDisplay(answer);
     }else if (operator == 'subtract') {
-        let answer =  subtract(a,b);
+        let answer =  subtract(argument1,argument2);
         return populateDisplay(answer);
     } else if (operator == 'multiply') {
-        let answer =  multiply(a,b);
+        let answer =  multiply(argument1,argument2);
         return populateDisplay(answer);
     } else if (operator == 'divide') {
-        let answer =  divide(a,b);
+        let answer =  divide(argument1,argument2);
         return populateDisplay(answer);
     }
 }
@@ -24,7 +24,7 @@ divide = (argument1,argument2) => argument1/argument2;
 function pressButton () {
     let number = ""
     let operator = "";
-    let arg1 = ""
+    let argument1 = ""
     const button = document.querySelectorAll('button');
     button.forEach((button) => {
         button.addEventListener('click', () => {
@@ -32,11 +32,11 @@ function pressButton () {
                 number += button.id
                 return populateDisplay(number);
             } else if (button.className == 'operator'){
-                arg1 = number;
+                argument1 = number;
                 operator = button.id;
                 number = "";
             } else if (button.id == 'equal'){
-                return operate(operator,arg1,number);
+                return operate(operator,argument1,number);
             }
         })
     })
